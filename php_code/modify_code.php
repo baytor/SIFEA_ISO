2c3cc51d43faacf57b6e598cff7d9c12be81e348
baytor <fierro.nic@gmail.com>
2021-09-08 16:56:18 +0200

    la pagina modify non è più utilizzata adesso si usa new per tutto

--- php_code/modify_code.php
+++ php_code/modify_code.php
@@ -1,76 +1,79 @@
-<?php
+<?php     //Potermmo usarla per confermare la modifica o la cancellazione delle pagine
 require_once('libs_code.php');
 session_start();
 
-if (isset($_GET['search']))
-{
-  action($_GET['search']);
-  $_SESSION['row_search'] = $_GET['search'];
-  echo "<br><br>Selezionato l'oggetto con chiave primaria = " . $_SESSION['row_search']."<br>";
-}
 
-if(isset($_POST['copia'])) //DA FARE
-{
-  // sarebbe utile che, dopo aver premuto copia, si vada su una pagina modify
-  // corrispondente all'oggetto copiato in modo da modificarlo subito
-  // e che magari abbia già la data di creazione impostata (ma anche no...)
-  
-  echo "<br><br>(_DA FARE_) Copiato l'oggetto con chiave primaria = " . $_SESSION['row_search']."<br>";
-}
 
-if(isset($_POST['elimina']))
-{
-  $_SESSION['entry1']->db_connection_on();
-  echo "<br><br>Eliminato l'oggetto con chiave primaria = " . $_SESSION['row_search']."<br>";
-  $_SESSION['entry1']->delete_row($_SESSION['row_search']);
-  $_SESSION['entry1']->db_connection_off();
-  echo "Redirecting in 2 seconds";
-  header("refresh:2; url=viewer.php");
-}
 
-if(isset($_POST['modifica'])) //DA FARE
-{
-  //modifica la voce selezionata
-  $_SESSION['entry1']->db_connection_on();
-  echo "<br><br>(_DA FARE_) Modifica l'oggetto con chiave primaria = " . $_SESSION['row_search']."<br>";
-
-  $_SESSION['entry1']->db_connection_off();
-}
-
-if(isset($_POST['aggiorna'])) //DA FARE
-{
-  $_SESSION['entry1']->db_connection_on();
-  //aggiorna una voce:
-  //  + copia la voce mettendola in revisione (es.: aumenta il n° rev, aggiorna la data proponendo quella odierna...)
-  //    dando la possibilità di modificare i campi già esistenti;
-  //  + mette inattiva (0) la voce vecchia
-
-  echo "<br><br>(_DA FARE_) Aggiorna l'oggetto con chiave primaria = " . $_SESSION['row_search']."<br>";
-  $_SESSION['entry1']->db_connection_off();
-}
-
-function action($search)
-{
-  //search($_GET['search']);
-  $_SESSION['entry1']->db_connection_on();
-  //$_SESSION['entry1']->select_rows_where_is($_SESSION['entry1']->clm_array[0],$search);
-  $_SESSION['entry1']->select_rows_by_string_by_pos("SELECT * FROM ".$_SESSION['entry1']->get_dbtable().
-  " WHERE ".$_SESSION['entry1']->get_clm_array_at(0)."=".$search.
-  " ORDER BY ".$_SESSION['entry1']->get_clm_array_at(15). " DESC",
-  1,count($_SESSION['entry1']->get_clm_array())-2);
-  $_SESSION['entry1']->db_connection_off();
-}
-
-echo "
-<div class='formdiv'>
-<form id=row_options method=post action=modify.php>
-<button type=submit name=copia>Copia</button>
-<button type=submit name=elimina>Elimina</button>
-</form>
-<form id=row_options method=post action=new.php>
-<button type=submit name=modifica>Modifica</button>
-<button type=submit name=aggiorna>Aggiorna</button>
-</form>
-</div>";
+// if (isset($_GET['search']))
+// {
+//   action($_GET['search']);
+//   $_SESSION['row_search'] = $_GET['search'];
+//   echo "<br><br>Selezionato l'oggetto con chiave primaria = " . $_SESSION['row_search']."<br>";
+// }
+//
+// if(isset($_POST['copia'])) //DA FARE
+// {
+//   // sarebbe utile che, dopo aver premuto copia, si vada su una pagina modify
+//   // corrispondente all'oggetto copiato in modo da modificarlo subito
+//   // e che magari abbia già la data di creazione impostata (ma anche no...)
+//
+//   echo "<br><br>(_DA FARE_) Copiato l'oggetto con chiave primaria = " . $_SESSION['row_search']."<br>";
+// }
+//
+// if(isset($_POST['elimina']))
+// {
+//   $_SESSION['entry1']->db_connection_on();
+//   echo "<br><br>Eliminato l'oggetto con chiave primaria = " . $_SESSION['row_search']."<br>";
+//   $_SESSION['entry1']->delete_row($_SESSION['row_search']);
+//   $_SESSION['entry1']->db_connection_off();
+//   echo "Redirecting in 2 seconds";
+//   header("refresh:2; url=viewer.php");
+// }
+//
+// if(isset($_POST['modifica'])) //DA FARE
+// {
+//   //modifica la voce selezionata
+//   $_SESSION['entry1']->db_connection_on();
+//   echo "<br><br>(_DA FARE_) Modifica l'oggetto con chiave primaria = " . $_SESSION['row_search']."<br>";
+//
+//   $_SESSION['entry1']->db_connection_off();
+// }
+//
+// if(isset($_POST['aggiorna'])) //DA FARE
+// {
+//   $_SESSION['entry1']->db_connection_on();
+//   //aggiorna una voce:
+//   //  + copia la voce mettendola in revisione (es.: aumenta il n° rev, aggiorna la data proponendo quella odierna...)
+//   //    dando la possibilità di modificare i campi già esistenti;
+//   //  + mette inattiva (0) la voce vecchia
+//
+//   echo "<br><br>(_DA FARE_) Aggiorna l'oggetto con chiave primaria = " . $_SESSION['row_search']."<br>";
+//   $_SESSION['entry1']->db_connection_off();
+// }
+//
+// function action($search)
+// {
+//   //search($_GET['search']);
+//   $_SESSION['entry1']->db_connection_on();
+//   //$_SESSION['entry1']->select_rows_where_is($_SESSION['entry1']->clm_array[0],$search);
+//   $_SESSION['entry1']->select_rows_by_string_by_pos("SELECT * FROM ".$_SESSION['entry1']->get_dbtable().
+//   " WHERE ".$_SESSION['entry1']->get_clm_array_at(0)."=".$search.
+//   " ORDER BY ".$_SESSION['entry1']->get_clm_array_at(15). " DESC",
+//   1,count($_SESSION['entry1']->get_clm_array())-2);
+//   $_SESSION['entry1']->db_connection_off();
+// }
+//
+// echo "
+// <div class='formdiv'>
+// <form id=row_options method=post action=modify.php>
+// <button type=submit name=copia>Copia</button>
+// <button type=submit name=elimina>Elimina</button>
+// </form>
+// <form id=row_options method=post action=new.php>
+// <button type=submit name=modifica>Modifica</button>
+// <button type=submit name=aggiorna>Aggiorna</button>
+// </form>
+// </div>";
 
 ?>
